c1d785a544091d80182363b9b373513356dfa383
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2018-10-01 18:04:36 +1000

    results from simulations

--- R/sc_PCA.R
+++ R/sc_PCA.R
@@ -62,3 +62,33 @@ plot_scpca_clus <- function(data, N) {
   plots
 }
 
+# Plots a series of single cell pca plots coloured by annotation
+plot_scpca_ann <- function(data, N, annotation, ann_label = "Annotation"){
+    samp <- sample(1:nrow(data), N, replace = FALSE)
+
+    # Peform the PCA on the subsampled data
+    pca <- prcomp(data[samp, 3:ncol(data)])$x
+
+    # Re-annotate the data
+    pca_plot <- data.frame(ann = factor(annotation[samp,]), pca)
+
+    # Create plots of different PC combinations
+    p_12 <- ggplot(pca_plot, aes(x = PC1, y = PC2, col = ann)) +
+      geom_point(alpha = 0.3) +
+      labs(col = ann_label) +
+      theme_bw()
+
+    p_23 <- ggplot(pca_plot, aes(x = PC2, y = PC3, col = ann)) +
+      geom_point(alpha = 0.3) +
+      labs(col = ann_label) +
+      theme_bw()
+
+    p_13 <- ggplot(pca_plot, aes(x = PC1, y = PC3, col = ann)) +
+      geom_point(alpha = 0.3) +
+      labs(col = ann_label) +
+      theme_bw()
+
+    plots <- list(p_12, p_23, p_13)
+    plots
+}
+
--- R/sc_tSNE.R
+++ R/sc_tSNE.R
@@ -1,9 +1,16 @@
 # Idea: compute tsne once, pass to all functions...
 
 # Take in data subsamples and converts it into annotated tsne form
-compute_tsne <- function(data, N){
-  samp_data <- sample_n(data, N, replace = FALSE)
-  tsne <- cytofkit::cytof_dimReduction(samp_data[3:ncol(samp_data)], method = "tsne", tsneSeed = 42)
+compute_tsne <- function(data, N = 1000, sample = NULL){
+  if (is.null(samp)){
+    # Sample internally
+    samp <- sample(1:nrow(data), N)
+    samp_data <- data[samp, ]
+  } else {
+    samp_data <- data[samp, ]
+  }
+  input <- samp_data[3:ncol(samp_data)]
+  tsne <- cytofkit::cytof_dimReduction(input, method = "tsne", tsneSeed = 42)
   colnames(tsne) <- c("tSNE1", "tSNE2")
   tsne_data <- data.frame(samp_data[,1:2], tsne)
   tsne_data
@@ -25,6 +32,18 @@ plot_tsne_cluster <- function(tsne_data){
   plot
 }
 
+# Plot a single cells tsne plot with a given annotation column
+# Note: The external user is reposible for correctly algining the annotation with
+# the data. This can be done by specifying a sample to passed into the original compute_tsne
+# function
+plot_tsne_ann <- function(tsne_data, annotation, ann_label = "Annotation"){
+  tsne_data <- data.frame(ann = factor(annotation), tsne_data)
+  plot <- ggplot(tsne_data, aes(x = tSNE1, y = tSNE2, col = ann)) +
+    geom_point(alpha = 0.3) +
+    labs(col = ann_label) +
+    theme_bw()
+  plot
+}
 
 plot_tsne_marker <- function(data, tsne_data){
   plots <- list()
